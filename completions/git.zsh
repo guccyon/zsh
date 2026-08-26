71295b2c963b7125c33906a0cbe81f023e743660
Tetsuro Higuchi <tetsuro-higuchi@c-fo.com>
2026-08-26 16:59:41 +0900

    Fix git worktree removal completion

--- completions/git.zsh
+++ completions/git.zsh
@@ -7,38 +7,49 @@ function _git-worktree-add-remote-branches() {
 }
 
 function _git-wr() {
-  local current_worktree record line worktree branch
-  local -a branches descriptions records
+  local common_dir base_dir current_worktree record line worktree relative_worktree head branch branch_display
+  local -a worktrees descriptions records
 
-  current_worktree=$(git rev-parse --show-toplevel 2>/dev/null)
+  common_dir=$(git rev-parse --git-common-dir 2>/dev/null) || return
+  base_dir=$(cd "$common_dir/.." && pwd -P) || return
+  current_worktree=$(git rev-parse --show-toplevel 2>/dev/null) || return
   records=( ${(ps.\n\n.)"$(git worktree list --porcelain 2>/dev/null)"} )
 
   for record in $records; do
     worktree=${${record%%$'\n'*}#worktree }
-    branch=""
+    [[ "$record" == *$'\nbare'* || "$worktree" == "$current_worktree" || "$worktree" != "$base_dir"/* ]] && continue
 
+    head=""
+    branch="(detached HEAD)"
     for line in ${(f)record}; do
-      if [[ $line == branch\ refs/heads/* ]]; then
-        branch=${line#branch refs/heads/}
-        break
-      fi
+      case "$line" in
+        HEAD\ *) head=${line#HEAD } ;;
+        branch\ refs/heads/*) branch=${line#branch refs/heads/} ;;
+      esac
     done
 
-    [[ -z "$branch" || "$worktree" == "$current_worktree" ]] && continue
-    branches+=("$branch")
-    descriptions+=("$worktree")
+    if [[ "$branch" == "(detached HEAD)" ]]; then
+      branch_display="$branch"
+    else
+      branch_display="[$branch]"
+    fi
+
+    relative_worktree=${worktree#"$base_dir/"}
+    worktrees+=("$relative_worktree")
+    descriptions+=("$branch_display  ${head[1,7]}  $worktree")
   done
 
-  local context state state_descr line
+  local context state state_descr
   _arguments -C -s -S \
     '(-f --force)'{-f,--force}'[remove a dirty or locked worktree]' \
-    '1:worktree branch:->branches'
+    '1:worktree path:->worktrees'
 
   case $state in
-    (branches)
-      (( ${#branches} )) && _wanted branches expl 'worktree branch' compadd -Q -d descriptions -a branches
+    (worktrees)
+      (( ${#worktrees} )) && _wanted worktrees expl 'worktree path' compadd -Q -l -d descriptions -a worktrees
     ;;
   esac
 }
 
 compdef _git-worktree-add-remote-branches git-worktree-add gwt
+compdef _git-wr git-wr
